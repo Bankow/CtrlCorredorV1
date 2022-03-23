e4fc72a9451cad6210fac54e386250043f02ba9e
Lucas Matias Bankow <lucas.bankow@poatek.com>
2022-03-23 20:18:00 -0300

    initial commit

---
+++ src/main/java/com/bcopstein/CtrlCorredorV1/CtrlCorredorV1Application.java
@@ -0,0 +1,29 @@
+package com.bcopstein.CtrlCorredorV1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+@ComponentScan(basePackages = { "com.bcopstein" })
+@EntityScan(basePackages = { "com.bcopstein" })
+public class CtrlCorredorV1Application {
+	public static void main(String[] args) {
+		SpringApplication.run(CtrlCorredorV1Application.class, args);
+	}
+	
+	@Bean
+	public Docket productApi() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.bcopstein.CtrlCorredorV1")).build();
+	}
+
+}
